b5af7d264d93585499bec29b6b9fc2c8411fd050
angelleeee <leeverde9@gmail.com>
2024-06-20 18:58:35 -0300

    Crear

--- src/pages/tipoPropiedad/NewTipoPropiedad.js
+++ src/pages/tipoPropiedad/NewTipoPropiedad.js
@@ -1,48 +1,57 @@
-    import React, { useState } from 'react';
-    import Header from '../../components/HeaderComponent';
-   
-    import FooterComponent from '../../components/FooterComponent';
-    import "../../assets/styles/NewTipoPropiedad.css"
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/HeaderComponent';
+import FooterComponent from '../../components/FooterComponent';
+import "../../assets/styles/NewTipoPropiedad.css"
 
-    const NewTipoPropiedad = () => {
+const NewTipoPropiedad = () => {
+  const [nombre, setNombre] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { nombre };
+    if (!nombre) {
+      alert('El campo no puede estar vacío.');
+      return;
+    }
+  fetch('http://localhost:80/tipos_propiedad', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error en la solicitud.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.message) {
+        setMensaje(data.message);
+        setNombre(''); 
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      setMensaje('El tipo de propiedad no puede repetirse'); // Mensaje genérico de error
+    });
+  };
 
-    const [message, setMessage] = useState('');
-    const [nombre, setNombre] = useState('');
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = { nombre };
-        if (!nombre) {
-          alert('El campo no puede estar vacío.')
-        }
-        fetch('http://localhost:80/tipos_propiedad', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-        .then((response) => response.json())
-        .catch((error) => {
-          console.log('Error:', error);
-        });
-      };
   return (
     <div>
       <Header/>
-      <form onSubmit={handleSubmit}>
-        <div>
-           <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
-        </div>
-        <button type="submit" id='crear' >Crear</button>
-      </form>
-      <button type="button" id="volver"><a href="http://localhost:3000/tipo_propiedad">Volver</a></button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+              {mensaje && <p>{mensaje}</p>} 
+          </div>
+          <button type="submit" id='crear'>Crear</button>
+        </form>
+        <button type="button" id="volver"><a href="http://localhost:3000/tipo_propiedad">Volver</a></button>
       <FooterComponent/>
-  </div>
+    </div>
   );
 };
 
-  
-
-export default NewTipoPropiedad;
+export default NewTipoPropiedad
